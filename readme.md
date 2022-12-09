0fceddb54ac8230ec908503fddda00fad3f7804f
Mephit299 <94972436+Mephit299@users.noreply.github.com>
2022-12-09 10:59:57 +0100

    Updaterade logbook och lade in en java class file

--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 Logbook 
+
+# Fredag Vecka 49
+* Vi har gjort ett pojekt och länkat det till github för vår första riktiga inlämningsuppgift.
+* Github vägrade först att akseptera mitt project fast det blev fixat.
---
+++ src/HengaGubbe.java
@@ -0,0 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class HengaGubbe {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner in = new Scanner(new File("giltigaOrd.txt"));
+    }
+}
