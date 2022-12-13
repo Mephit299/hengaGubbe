bfec8d5428c2e3a01480468170ea36a7853c8808
Mephit299 <94972436+Mephit299@users.noreply.github.com>
2022-12-13 14:26:52 +0100

    Updaterade logbook och lade in en java class file

--- src/HengaGubbe.java
+++ src/HengaGubbe.java
@@ -5,5 +5,11 @@ import java.util.Scanner;
 public class HengaGubbe {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner in = new Scanner(new File("giltigaOrd.txt"));
+        int ord = (int) (Math.random()*101);
+        String[] ordet = new String[ord];
+        for (int x=1;x<ord; x++)
+            ordet[x]= in.nextLine();
+        String rättOrd = ordet[ord];
+
     }
 }
