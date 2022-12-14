3e16770bc4334d7ee4fb29e6173372787c586f90
Mephit299 <94972436+Mephit299@users.noreply.github.com>
2022-12-14 10:55:21 +0100

    Gjort klart listan

--- src/HengaGubbe.java
+++ src/HengaGubbe.java
@@ -1,30 +1,29 @@
-import javax.swing.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class HengaGubbe {
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner in = new Scanner(new File("giltigaOrd.txt"));
+        Scanner inList = new Scanner(new File("giltigaOrd.txt"));
+        Scanner inGissning = new Scanner(System.in);
         int ord = (int) (Math.random()*101);
         String ordet = "";
         for (int x = 0; x<ord; x++)
-           ordet = in.nextLine();
+           ordet = inList.nextLine();
         String txt ="";
-        System.out.println(ord);
-        System.out.println(ordet);
         int ordLenght = ordet.length();
         for (int x = 0; x<ordLenght; x++)
             txt = txt + "_";
+        System.out.println(ord);
         while (true) {
-            String gissning = JOptionPane.showInputDialog("Gissning " +txt);
+            System.out.println("Gissning " + txt);
+            String gissning = inGissning.nextLine();
             String svar = Levi.title(ordet, gissning, txt);
-            System.out.println(svar);
-            System.out.println(txt);
             if (svar.equals("false")){}
             else
                 txt = svar;
             if (txt.toLowerCase().equals(ordet)){
+                System.out.println(txt);
                 System.out.println("rätt");
                 System.exit(0);
             }
@@ -32,4 +31,6 @@ public class HengaGubbe {
 
         }
     }
+
+
 }
