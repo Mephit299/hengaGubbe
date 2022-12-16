b2c7cac1f03baf46d3a7b33289a5cd9a9d0cdeec
Pissoarsniffare <94972482+Pissoarsniffare@users.noreply.github.com>
2022-12-16 10:43:47 +0100

    skrev kod som kontrollerar gissningen

--- src/Samuel.java
+++ src/Samuel.java
@@ -1,2 +1,18 @@
 public class Samuel {
+    public static String gissat(String gissat, String gissning, String gissadeord) {
+        if (gissat.contains(gissning)){return "bokstavgissat";}
+        else if (gissadeord.contains(gissning)) {return "ordgissat";}
+
+        if (gissning.length()<=1) {
+            gissat = gissat + gissning;
+            return gissat;
+        }
+        else {
+            gissadeord = gissadeord+gissat;
+            return gissadeord;
+        }
+
+    }
+
+
 }
