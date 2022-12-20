9a38e23cbed98e17ee9e0071a7147db24e65fece
Pissoarsniffare <94972482+Pissoarsniffare@users.noreply.github.com>
2022-12-20 14:54:42 +0100

    jag hängde en gubbe

--- src/Samuel.java
+++ src/Samuel.java
@@ -1,7 +1,7 @@
 public class Samuel {
     public static String gissat(String gissat, String gissning, String gissadeord) {
         if (gissat.contains(gissning)){return "bokstavgissat";}
-        else if (gissadeord.contains(gissning)) {return "ordgissat";}
+        else if (gissadeord.contains(gissning) && gissning.length()>1) {return "ordgissat";}
 
         if (gissning.length()<=1) {
             gissat = gissat + gissning;
@@ -14,5 +14,175 @@ public class Samuel {
 
     }
 
+    public static String gubbe(int antalgissningar){
+        String gubbe;
+
+         if (antalgissningar == 0) {
+             gubbe = (
+
+                     "     +---+  \n" +
+                             "     |   |  \n" +
+                             "     O   |  \n" +
+                             "    /|\\  |  \n" +
+                             "    / \\  |  \n" +
+                             "         |  \n" +
+                             "     ____|____ \n" +
+                             "    /         \\ \n" +
+                             " ～～～～～～～～～～～"
+             );
+             return gubbe;
+         }
+        else if (antalgissningar == 10) {
+             gubbe = (
+
+                     "   \n" +
+                             "  \n" +
+                             "  \n" +
+                             "  \n" +
+                             "  \n" +
+                             "  \n" +
+                             "  \n" +
+                             "  \n" +
+                             " ～～～～～～～～～～～"
+             );
+             return gubbe;
+         }
+
+        else if (antalgissningar == 9) {
+             gubbe = (
+                     "  \n" +
+                             "    \n" +
+                             "    \n" +
+                             "    \n" +
+                             "    \n" +
+                             "    \n" +
+                             "     _________ \n" +
+                             "    /         \\ \n" +
+                             " ～～～～～～～～～～～"
+             );
+             return gubbe;
+         }
+
+        else if (antalgissningar == 8) {
+             gubbe = (
+                     "   \n" +
+                             "         |  \n" +
+                             "         |  \n" +
+                             "         |  \n" +
+                             "         |  \n" +
+                             "         |  \n" +
+                             "     ____|____ \n" +
+                             "    /         \\ \n" +
+                             " ～～～～～～～～～～～"
+             );
+             return gubbe;
+         }
+
+         else if (antalgissningar == 7) {
+             gubbe = (
+                     "     +---+  \n" +
+                             "         |  \n" +
+                             "         |  \n" +
+                             "         |  \n" +
+                             "         |  \n" +
+                             "         |  \n" +
+                             "     ____|____ \n" +
+                             "    /         \\ \n" +
+                             " ～～～～～～～～～～～"
+             );
+             return gubbe;
+         }
+
+         else if (antalgissningar == 6) {
+             gubbe = (
+                     "     +---+  \n" +
+                             "     |   |  \n" +
+                             "         |  \n" +
+                             "         |  \n" +
+                             "         |  \n" +
+                             "         |  \n" +
+                             "     ____|____ \n" +
+                             "    /         \\ \n" +
+                             " ～～～～～～～～～～～"
+             );
+             return gubbe;
+         }
+
+         else if (antalgissningar == 5) {
+             gubbe = (
+                     "     +---+  \n" +
+                             "     |   |  \n" +
+                             "     O   |  \n" +
+                             "         |  \n" +
+                             "         |  \n" +
+                             "         |  \n" +
+                             "     ____|____ \n" +
+                             "    /         \\ \n" +
+                             " ～～～～～～～～～～～"
+             );
+             return gubbe;
+         }
+
+         else if (antalgissningar == 4) {
+             gubbe = (
+                     "     +---+  \n" +
+                             "     |   |  \n" +
+                             "     O   |  \n" +
+                             "     |   |  \n" +
+                             "         |  \n" +
+                             "         |  \n" +
+                             "     ____|____ \n" +
+                             "    /         \\ \n" +
+                             " ～～～～～～～～～～～"
+             );
+             return gubbe;
+         }
+
+         else if (antalgissningar == 3) {
+             gubbe = (
+                     "     +---+  \n" +
+                             "     |   |  \n" +
+                             "     O   |  \n" +
+                             "    /|   |  \n" +
+                             "         |  \n" +
+                             "         |  \n" +
+                             "     ____|____ \n" +
+                             "    /         \\ \n" +
+                             " ～～～～～～～～～～～"
+             );
+             return gubbe;
+         }
+
+         else if (antalgissningar == 2) {
+             gubbe = (
+                     "     +---+  \n" +
+                             "     |   |  \n" +
+                             "     O   |  \n" +
+                             "    /|\\  |  \n" +
+                             "         |  \n" +
+                             "         |  \n" +
+                             "     ____|____ \n" +
+                             "    /         \\ \n" +
+                             " ～～～～～～～～～～～"
+             );
+             return gubbe;
+         }
+
+         else if (antalgissningar == 1) {
+             gubbe = (
+                     "     +---+  \n" +
+                             "     |   |  \n" +
+                             "     O   |  \n" +
+                             "    /|\\  |  \n" +
+                             "    /    |  \n" +
+                             "         |  \n" +
+                             "     ____|____ \n" +
+                             "    /         \\ \n" +
+                             " ～～～～～～～～～～～"
+             );
+             return gubbe;
+         }
+         return "";
+    }
 
 }
